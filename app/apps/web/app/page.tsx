e2b8a2ec1b3281c3d4903d0f1cc67d2e4c6f9aae
ebijun1007@gmail.com <ebijun1007@gmail.com>
2023-12-08 09:09:21 +0900

    wip

--- app/apps/web/app/page.tsx
+++ app/apps/web/app/page.tsx
@@ -5,10 +5,10 @@ import { Button } from "ui";
 import styles from "../styles/index.module.css";
 
 export default function Web() {
-  return (
-    <div className={styles.container}>
-      <h1>Web</h1>
-      <Button onClick={() => console.log("Pressed!")} text="Boop" />
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h1>Web</h1>
+			<Button onClick={() => console.log("Pressed!")} text="Boop" />
+		</div>
+	);
 }
